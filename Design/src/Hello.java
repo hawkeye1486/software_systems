0b953b6f2588bbd45b887b9df4391030420ce1e9
hawkeye1486 <hawkeye1485@gmail.com>
2016-12-11 13:52:43 +0100

    Burn^2

--- Design/src/Hello.java
+++ Design/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Jou moeder is zo groot, ze heeft haar eigen postcode");
+		System.out.println("Jou moeder is zo groot! Ze heeft haar eigen postcode");
 	}
 
 }
