385b231ad219617324df5ccf3df9ddf0fa9b1fa2
hawkeye1486 <hawkeye1485@gmail.com>
2016-12-11 13:47:42 +0100

    BUUUURN

--- Design/src/Hello.java
+++ Design/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello bitch");
+		System.out.println("Jou moeder is zo groot, ze heeft haar eigen postcode");
 	}
 
 }
