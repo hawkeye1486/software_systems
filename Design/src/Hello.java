d25498c0d7f068d1ae1ca526527164da04d1e854
hawkeye1486 <hawkeye1485@gmail.com>
2016-12-12 11:02:45 +0100

    X2

--- Design/src/Hello.java
+++ Design/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2 woorden 9 letters");
-		System.out.println("bier halen");
+		System.out.println("bier halen!");
 	}
 		
 }
