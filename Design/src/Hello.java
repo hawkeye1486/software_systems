ec0acbc27a91d3e6c1a9bbaf89f04400225873ff
hawkeye1486 <hawkeye1485@gmail.com>
2016-12-12 11:00:09 +0100

    bier halen in Hallo

--- Design/src/Hello.java
+++ Design/src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello bitch");
+		System.out.println("2 woorden 9 letters");
+		System.out.println("bier halen");
 	}
-
+		
 }
